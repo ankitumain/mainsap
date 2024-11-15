2343ba9b789884d2d1f0c5886a7766687e14e2cc
ankitumain <ankit.mittal@umain.com>
2024-11-15 18:14:43 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
-import AnimatedButton from "./components/AnimateButton";
-import AnimatedSection from "./components/AnimateSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       {/* Main Content */}
-      <main className="flex-grow bg-gray-100 p-6">
-        <div className="container mx-auto text-center">
-          <AnimatedButton>Click Me</AnimatedButton>
-          <AnimatedSection />
-        </div>
+      <main className="flex-grow bg-black p-6">
+        <div className="container mx-auto text-center"></div>
       </main>
       <Footer />
     </div>
