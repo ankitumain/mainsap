1edbe66fa66497ac149618e94ce619a1d4b83413
ViktoriiaUmain <viktoriia.safonova@umain.com>
2024-11-15 19:07:41 +0100

    add custom font and update Text component styling

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -4,7 +4,7 @@ type TextProps = {
 
 export default function Text({ children }: TextProps) {
   return (
-    <section className="py-96 flex justify-end">
+    <section className="py-96 flex justify-end body-40">
       <div className="w-2/3">{children}</div>
     </section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,17 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        "khteka-medium": ["KHTeka Medium", "sans-serif"],
+      },
+      letterSpacing: {
+        "neg-6p": "-0.06em",
+        "neg-5p": "-0.05em",
+        "neg-4p": "-0.04em",
+        "neg-3p": "-0.03em",
+        "neg-2p": "-0.02em",
+        "neg-1p": "-0.01em",
+      },
     },
   },
   plugins: [],
