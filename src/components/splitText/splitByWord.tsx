0b5e3b2f284c9a84b83f70dcb7bb3f97995db095
Anton Strandqvist <anton.strandqvist@umain.com>
2024-11-15 20:14:57 +0100

    Added FadeIn

--- src/components/splitText/splitByWord.tsx
+++ src/components/splitText/splitByWord.tsx
@@ -1,14 +1,22 @@
 'use client'
+import { useState } from "react"
 import SplitType from "split-type"
 import { useRef, useEffect } from "react"
-export function SplitByWord({children} : {children: React.ReactNode}) {
+export function SplitByWord({children, callback} : {children: React.ReactNode, callback?: () => void}) {
     const ref = useRef<HTMLDivElement>(null);
+    const [splitted, setSplitted] = useState(false);
     useEffect(() => {
         if (!ref.current) return
-        new SplitType(ref.current, {types: 'words', tagName: 'span'})
-    }, [])
+        const split = new SplitType(ref.current, {types: 'words', tagName: 'span', wordClass: 'word opacity-0'})
+        setSplitted(true)
+        if (callback) callback()
+        return () => {
+            split.revert()
+        }
+        
+    }, [callback])
     return (
-        <div ref={ref} className="">
+        <div ref={ref} className={`${splitted ? 'opacity-100' : 'opacity-0'} `}>
             {children}
         </div>
     )
