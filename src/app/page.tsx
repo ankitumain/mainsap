1afef43ab02e95d4026aa2d529ff597eccb9fe58
Tomo Myrman <tomo.myrman@umain.com>
2024-11-15 21:00:54 +0100

    adding global font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import HorizontalScroll from "@/components/text/animations/HorizontalScroll";
 
-import {FadeIn} from "@/components/AnimatedText/FadeIn";
+import { FadeIn } from "@/components/AnimatedText/FadeIn";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-khteka">
       <Header />
       <FadeIn>
         <h1 className="text-6xl font-bold text-center">Welcome to mainsap</h1>
