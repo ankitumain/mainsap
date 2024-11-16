f2ceae5b6d7d1185637969ff50860bc3af11484b
Anton Strandqvist <anton.strandqvist@umain.com>
2024-11-16 14:30:01 +0100

    added counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Intro } from "@/components/intro";
 import FadeInText from "@/components/AnimatedText/FadeInText";
 import HorizontalScroll from "@/components/animations/HorizontalScroll";
 import CustomCursor from "@/components/customCursor/CustomCursor";
+import SlotCounter from 'react-slot-counter';
 
 const horizontalScrollTexts = [
   {
@@ -26,6 +27,13 @@ export default function Home() {
       <Intro />
       {/* Main Content */}
       <main className="flex-grow bg-transparent text-white z-10 pb-[100vh]">
+        <section>
+        <div className="h-svh w-full flex items-center bg-[#EF4123] font-khteka px-12">
+          <div className="flex items-start">
+            <h2 className="text-[400px] tracking-tighter"><SlotCounter animateOnVisible value={2019} duration={2} /></h2>
+
+          </div>
+        </div>
         <FadeInText className="bg-yellow">
           <p>
             Kurppa Hosk, Animal, Keybroker and Curamando come together to build
@@ -35,6 +43,7 @@ export default function Home() {
           </p>
         </FadeInText>
         <HorizontalScroll paragraphs={[horizontalScrollTexts[0]]} />
+        </section>
         <FadeInText className="bg-dark-blue">
           <p>
             ARC Arise Consulting Group launches and Cupole joins the family.
