52e674144d7f8c9c1271224839793e83d201fded
Tomo Myrman <tomo.myrman@umain.com>
2024-11-16 13:59:42 +0100

    footer copy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,9 @@ export default function Footer() {
     <footer className="h-screen w-screen z-0 fixed bg-black text-white p-4 font-khteka">
       <div className="container h-full w-full flex flex-col items-center justify-center mx-auto">
         <h1 className="text-[200px] text-center">Fin.</h1>
-        <p>Made by Umain.</p>
+        <div className="text-2xl">
+          <p>Made by Umain. ISO certified.</p>
+        </div>
       </div>
     </footer>
   );
