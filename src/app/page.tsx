b41b03a2d74f1c992b3a376ab124c4cf2fc05b70
ViktoriiaUmain <viktoriia.safonova@umain.com>
2024-11-16 09:50:58 +0100

    refactor: import, according changed folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
-import Text from "@/components/text/Text";
-import HorizontalScroll from "@/components/text/animations/HorizontalScroll";
-
-import { FadeIn } from "@/components/AnimatedText/FadeIn";
+import FadeInText from "@/components/AnimatedText/FadeInText";
+import HorizontalScroll from "@/components/animations/HorizontalScroll";
 
 export default function Home() {
   return (
@@ -11,66 +9,54 @@ export default function Home() {
       <Header />
       {/* Main Content */}
       <main className="flex-grow bg-black text-white">
-        <Text className="bg-yellow">
-          <FadeIn>
-            <p>
-              Kurppa Hosk, Animal, Keybroker and Curamando come together to
-              build a new challenger. 300 pioneering management consultants and
-              digital experts – brought together to make our clients winners in
-              a transformative landscape.
-            </p>
-          </FadeIn>
-        </Text>
+        <FadeInText className="bg-yellow">
+          <p>
+            Kurppa Hosk, Animal, Keybroker and Curamando come together to build
+            a new challenger. 300 pioneering management consultants and digital
+            experts – brought together to make our clients winners in a
+            transformative landscape.
+          </p>
+        </FadeInText>
         <HorizontalScroll />
-        <Text className="bg-dark-blue">
-          <FadeIn>
-            <p>
-              ARC Arise Consulting Group launches and Cupole joins the family.
-            </p>
-          </FadeIn>
-        </Text>
-        <Text className="bg-red">
-          <FadeIn>
-            <p>
-              At the beginning of the year, Nameless.today (later part of Above)
-              is founded. Umain and Above join the journey in the fall. The
-              three companies lay the foundation for the Innovation, Tech and
-              Data capability.
-            </p>
-          </FadeIn>
-        </Text>
-        <Text className="bg-dark-blue">
-          <FadeIn>
-            <p>
-              The group is further established in Norway through acquisition of
-              Heydays and Goods, and also expands into Europe with Fabrique and
-              Q42 joining, established in the Netherlands.
-            </p>
-          </FadeIn>
-        </Text>
-        <Text className="bg-black">
-          <FadeIn>
-            <p className="mb-10">
-              Proof of concept in Europe and expanding into the US
-            </p>
-            <p>
-              Consulting offer in the Netherlands is launched, building a proof
-              of concept in Europe. Simultaneously, the group expands into the
-              US with value offer expansion primarily within Brand, Marketing
-              and Experience, and Innovation, Technology and Data.
-            </p>
-          </FadeIn>
-        </Text>
-        <Text className="bg-dark-blue">
-          <FadeIn>
-            <p className="mb-10">ARC becomes Eidra</p>
-            <p>
-              Eidra launches, reflecting the continued journey ahead. Eidra is a
-              manifestation of bigger ambitions, more members of the consultancy
-              collective, and an ambition to create great change.
-            </p>
-          </FadeIn>
-        </Text>
+        <FadeInText className="bg-dark-blue">
+          <p>
+            ARC Arise Consulting Group launches and Cupole joins the family.
+          </p>
+        </FadeInText>
+        <FadeInText className="bg-red">
+          <p>
+            At the beginning of the year, Nameless.today (later part of Above)
+            is founded. Umain and Above join the journey in the fall. The three
+            companies lay the foundation for the Innovation, Tech and Data
+            capability.
+          </p>
+        </FadeInText>
+        <FadeInText className="bg-dark-blue">
+          <p>
+            The group is further established in Norway through acquisition of
+            Heydays and Goods, and also expands into Europe with Fabrique and
+            Q42 joining, established in the Netherlands.
+          </p>
+        </FadeInText>
+        <FadeInText className="bg-black">
+          <p className="mb-10">
+            Proof of concept in Europe and expanding into the US
+          </p>
+          <p>
+            Consulting offer in the Netherlands is launched, building a proof of
+            concept in Europe. Simultaneously, the group expands into the US
+            with value offer expansion primarily within Brand, Marketing and
+            Experience, and Innovation, Technology and Data.
+          </p>
+        </FadeInText>
+        <FadeInText className="bg-dark-blue">
+          <p className="mb-10">ARC becomes Eidra</p>
+          <p>
+            Eidra launches, reflecting the continued journey ahead. Eidra is a
+            manifestation of bigger ambitions, more members of the consultancy
+            collective, and an ambition to create great change.
+          </p>
+        </FadeInText>
         <div className="container mx-auto text-center"></div>
       </main>
       <Footer />
---
+++ src/components/AnimatedText/FadeInText.tsx
@@ -0,0 +1,16 @@
+import { FadeIn } from "../animations/FadeIn";
+
+type TextProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function FadeInText({ children, className }: TextProps) {
+  return (
+    <section className={`py-96 px-14 flex justify-end body-40 ${className}`}>
+      <div className="w-2/3">
+        <FadeIn>{children}</FadeIn>
+      </div>
+    </section>
+  );
+}
